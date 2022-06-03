f0ca4960add04e973357a29bf2f2d9f550d0d8ab
teely44y <ju.tannatee2543@gmail.com>
2022-06-03 18:53:48 +0700

    refactor: presence data

--- src/MainDiscordBot.ts
+++ src/MainDiscordBot.ts
@@ -1,16 +1,30 @@
 import DiscordBot from '@src/DiscordBot';
-import { Interaction } from 'discord.js';
+import { Interaction, PresenceData } from 'discord.js';
 import PingCommand from '@src/commands/common/PingCommand';
 import HelpCommand from '@src/commands/common/HelpCommand';
+import LogInfo from '@utils/LogInfo';
 
 class MainDiscordBot extends DiscordBot {
 
+  private presence: PresenceData = {
+    status: 'online',
+    activities: [{
+      name: 'Github',
+      type: 'PLAYING',
+    }],
+  };
+
   protected async onReady(): Promise<void> {
-    console.info(`Logged in as ${this.client.user.tag} !`);
+    LogInfo.client(`Logged in as ${this.client.user.tag} !`);
+
+    const setPresence = () => this.client.user.setPresence(this.presence);
+
+    setPresence();
+    setInterval(setPresence, 5 * 60 * 1000);
 
     this.commandManager.registerCommand([
       new PingCommand(),
-      new HelpCommand(this.client),
+      new HelpCommand(this),
     ]);
   }
 
